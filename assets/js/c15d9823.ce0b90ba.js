e20835948bdf9b3a05a3a3a9b0ce958a3f891b41
bluswtr <bluswtr@gmail.com>
2025-04-10 12:51:37 -0700

    v2

---
+++ assets/js/c15d9823.ce0b90ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkworkportfolio_2=self.webpackChunkworkportfolio_2||[]).push([[146],{9328:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
